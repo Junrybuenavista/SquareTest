d51edd2c6446e093071f77e10fb35d7d79064ba6
Junrybuenavista <42229618+Junrybuenavista@users.noreply.github.com>
2021-10-29 09:01:44 +0800

    Update Query.php

--- Query.php
+++ Query.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "amazon";
+$dbname = "square_db";
 $msg = "";
 $msg.= "<style>
 table, td, th {
@@ -26,47 +26,52 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 //echo $_GET['lastweek']."  ".$_GET['currentday'];
-$sql = "SELECT Order_Date, Order_ID, Title, Item_Subtotal, Item_Quantity, Location FROM amazon_data WHERE (Item_Tax = '' OR Item_Tax = '0') AND Order_Date between '".$_GET['lastweek']."' and '".$_GET['currentday']."'  ORDER BY Order_Date";
-$result = $conn->query($sql);
+$msg.= "<center><h1>Square Report</h1></center>";
 
-$msg.= "<center><h1>Amazon Sales Tax Report</h1></center>";
-$msg.= "<center><h3>Date: ".date("m-d-Y")."</h3></center>";
 
-if ($result->num_rows > 0) {
-  $msg.= "<table>
-  <tr>
-    <th>Order Date</th>
-	<th>Location</th>
-    <th>Order Number</th>
-    <th>Item Name</th>
-	<th>Item Price</th>
-	<th>QTY</th>
-	<th>Estimated Tax</th>
-  </tr>";
-  while($row = $result->fetch_assoc()) {
-	
-	$total = $row["Item_Subtotal"];
-	$quantity = $row["Item_Quantity"];
-	$price = $total/$quantity;
-	$estTax = $total * 0.07;
-	
-	$msg.= " 
-			<tr>
-			<td>".$row["Order_Date"]."</td>
-			<td>".$row["Location"]."</td>
-			<td>".$row["Order_ID"]."</td>
-			<td>".$row["Title"]."</td>
-			<td>".number_format((float)$price, 2, '.', '')."</td>
-			<td>".$row["Item_Quantity"]."</td>
-			<td>".number_format((float)$estTax, 2, '.', '')."</td>
-		  </tr>
-		 ";
-	
-  }
-} else {
-  $msg.= "<center><h3>No result</h3></center>";
-}
-$msg.= "</table>";
+$array = array("Plaza Bistro","@theMarket");
+foreach($array as $location){
+		$sql = "SELECT DISTINCT Category FROM `square_data` where Location = '".$location."'";
+		$result = $conn->query($sql);
+
+		$msg.= "<center><h3>Location: ".$location."</h3></center>";
+
+		if ($result->num_rows > 0) {
+		  $msg.= "<table>
+		  <tr>
+			<th>Category</th>
+			<th>Item Sold</th>
+			<th>Gross Sales</th>
+		  </tr>";
+		  while($row = $result->fetch_assoc()) {
+			
+			$category = $row["Category"];
+				
+				$sql2 = "SELECT Qty FROM `square_data` where Location = '".$location."' AND Category = '".$category."'";
+				$result2 = $conn->query($sql2);
+				$Qty = 0;
+				if ($result2->num_rows > 0) {
+					while($row = $result2->fetch_assoc()){
+					$Qty += $row["Qty"];
+				
+					}
+				}
+			
+			
+			$msg.= " 
+					<tr>
+					<td>".$category."</td>
+					<td>".$Qty."</td>
+					<td></td>
+				  </tr>
+				 ";
+			
+		  }
+		} else {
+		  $msg.= "<center><h3>No result</h3></center>";
+		}
+		$msg.= "</table>";
+}		
 
 echo $msg;
 
